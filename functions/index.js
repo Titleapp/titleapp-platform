821fbde2cf0acded1a886c77fde3b8428de10e06
Sean Lee Combs <seancombs@MacBook-Air-84.lan>
2026-01-11 20:04:20 -1000

    Flatten functions directory

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,3 @@
-const functions = require("firebase-functions");
-
-exports.api = functions.https.onRequest((req, res) => {
-  res.status(200).json({
-    ok: true,
-    message: "API executor is live",
-    path: req.path,
-    method: req.method
-  });
-});
+exports.api = (req, res) => {
+  res.json({ ok: true, message: "API executor is live" });
+};
