2ead1a42222a855a3a68ac226aa0f04f65687f0e
Titleapp <dev@titleapp.io>
2026-01-11 19:59:39 -1000

    Refactor API to use Firebase functions
    Updated API function to use Firebase functions for better handling.

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,10 @@
-exports.api = (req, res) => {
-  res.json({ ok: true, message: "API executor is live" });
-};
+const functions = require("firebase-functions");
+
+exports.api = functions.https.onRequest((req, res) => {
+  res.status(200).json({
+    ok: true,
+    message: "API executor is live",
+    path: req.path,
+    method: req.method
+  });
+});
